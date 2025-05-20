7dc24aa00e40dc367074e2b39ba5398583466771
Zaenal Arif <zaenalariftech@gmail.com>
2025-05-21 02:36:03 +0700

    Add sorting and delete actions to ListAppBar with corresponding UI components

--- app/src/main/java/com/mobbelldev/todocompose/ui/screens/ListAppBar.kt
+++ app/src/main/java/com/mobbelldev/todocompose/ui/screens/ListAppBar.kt
@@ -1,23 +1,42 @@
 package com.mobbelldev.todocompose.ui.screens
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.mobbelldev.todocompose.R
+import com.mobbelldev.todocompose.components.PriorityItem
+import com.mobbelldev.todocompose.data.model.Priority
+import com.mobbelldev.todocompose.ui.theme.LARGE_PADDING
 
 @Composable
 fun ListAppBar() {
     DefaultListAppBar(
         onSearchClicked = {
 
-        }
+        },
+        onSortClicked = {
+
+        },
+        
+        onDeleteAction = {
+
+        },
     )
 }
 
@@ -25,6 +44,8 @@ fun ListAppBar() {
 @Composable
 fun DefaultListAppBar(
     onSearchClicked: () -> Unit,
+    onSortClicked: (Priority) -> Unit,
+    onDeleteAction: () -> Unit,
 ) {
     TopAppBar(
         title = {
@@ -32,7 +53,9 @@ fun DefaultListAppBar(
         },
         actions = {
             ListAppBarActions(
-                onSearchClicked = onSearchClicked
+                onSearchClicked = onSearchClicked,
+                onSortClicked = onSortClicked,
+                onDeleteAction = onDeleteAction,
             )
         }
     )
@@ -41,9 +64,19 @@ fun DefaultListAppBar(
 @Composable
 fun ListAppBarActions(
     onSearchClicked: () -> Unit,
+    onSortClicked: (Priority) -> Unit,
+    onDeleteAction: () -> Unit,
 ) {
     SearchAction(
         onSearchClicked = onSearchClicked
+    )
+
+    SortAction(
+        onSortClicked = onSortClicked
+    )
+
+    DeleteAllAction(
+        onDeleteAction = onDeleteAction
     )
 }
 
@@ -64,12 +97,99 @@ fun SearchAction(
     }
 }
 
+@Composable
+fun SortAction(onSortClicked: (Priority) -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+
+    IconButton(onClick = { expanded = true }) {
+        Icon(
+            painter = painterResource(R.drawable.ic_filter_list_24),
+            contentDescription = stringResource(R.string.sort_action),
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {
+                expanded = false
+            },
+        ) {
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.LOW)
+                },
+                text = {
+                    PriorityItem(Priority.LOW)
+                },
+            )
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.HIGH)
+                },
+                text = {
+                    PriorityItem(Priority.HIGH)
+                },
+            )
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.NONE)
+                },
+                text = {
+                    PriorityItem(Priority.NONE)
+                },
+            )
+        }
+    }
+}
+
+@Composable
+fun DeleteAllAction(
+    onDeleteAction: () -> Unit,
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    IconButton(onClick = { expanded = true }) {
+        Icon(
+            painter = painterResource(R.drawable.ic_more_vert_24),
+            contentDescription = stringResource(R.string.delete_all_action),
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {
+                expanded = false
+            },
+        ) {
+            DropdownMenuItem(
+                text = {
+                    Text(
+                        modifier = Modifier.padding(start = LARGE_PADDING),
+                        text = stringResource(R.string.delete_all_action),
+                    )
+                },
+                onClick = {
+                    expanded = false
+                    onDeleteAction()
+                },
+            )
+        }
+    }
+}
+
 @Preview
 @Composable
 private fun DefaultListAppBarPreview() {
     DefaultListAppBar(
         onSearchClicked = {
 
-        }
+        },
+        onSortClicked = {
+
+        },
+        onDeleteAction = {
+
+        },
     )
 }

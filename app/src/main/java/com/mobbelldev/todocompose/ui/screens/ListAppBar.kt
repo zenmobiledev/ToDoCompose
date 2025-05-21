4164a106487de4aae9f4c1346215b843a502555a
Zaenal Arif <zaenalariftech@gmail.com>
2025-05-21 08:03:18 +0700

    Add SearchAppBar component to ListAppBar with UI elements and functionality

--- app/src/main/java/com/mobbelldev/todocompose/ui/screens/ListAppBar.kt
+++ app/src/main/java/com/mobbelldev/todocompose/ui/screens/ListAppBar.kt
@@ -1,14 +1,22 @@
 package com.mobbelldev.todocompose.ui.screens
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,27 +24,39 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import com.mobbelldev.todocompose.R
 import com.mobbelldev.todocompose.components.PriorityItem
 import com.mobbelldev.todocompose.data.model.Priority
 import com.mobbelldev.todocompose.ui.theme.LARGE_PADDING
+import com.mobbelldev.todocompose.ui.theme.TOP_APP_BAR_HEIGHT
+import com.mobbelldev.todocompose.ui.theme.TOP_BAR_ELEVATION
 
 @Composable
 fun ListAppBar() {
-    DefaultListAppBar(
-        onSearchClicked = {
+//    DefaultListAppBar(
+//        onSearchClicked = {
+//
+//        },
+//        onSortClicked = {
+//
+//        },
+//
+//        onDeleteAction = {
+//
+//        },
+//    )
 
-        },
-        onSortClicked = {
-
-        },
-        
-        onDeleteAction = {
-
-        },
+    SearchAppBar(
+        text = "",
+        onTextChange = {},
+        onClosedClicked = {},
+        onSearchClicked = {}
     )
 }
 
@@ -178,6 +198,73 @@ fun DeleteAllAction(
     }
 }
 
+@Composable
+fun SearchAppBar(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onClosedClicked: () -> Unit,
+    onSearchClicked: (String) -> Unit,
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(TOP_APP_BAR_HEIGHT),
+        shadowElevation = TOP_BAR_ELEVATION,
+    ) {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = text,
+            onValueChange = {
+                onTextChange(it)
+            },
+            placeholder = {
+                Text(
+                    modifier = Modifier.alpha(0.5F),
+                    text = "Search"
+                )
+            },
+            singleLine = true,
+            leadingIcon = {
+                IconButton(
+                    modifier = Modifier.alpha(alpha = 0.5F),
+                    onClick = {
+
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = "Search Icon",
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = { onClosedClicked() }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = "Close Icon",
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search,
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onSearchClicked(text)
+                },
+            ),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+            )
+        )
+    }
+}
+
 @Preview
 @Composable
 private fun DefaultListAppBarPreview() {
@@ -191,5 +278,16 @@ private fun DefaultListAppBarPreview() {
         onDeleteAction = {
 
         },
+    )
+}
+
+@Preview
+@Composable
+private fun SearchAppBarPreview() {
+    SearchAppBar(
+        text = "",
+        onTextChange = {},
+        onClosedClicked = {},
+        onSearchClicked = {}
     )
 }
